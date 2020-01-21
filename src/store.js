a6c2d7f6d01e348dd061ecd5f03918a386c2e726
EricRubio589 <eric.rubio.services@gmail.com>
2020-01-20 22:16:36 -0600

    Server organized in modules

---
+++ src/store.js
@@ -0,0 +1,9 @@
+const bookmarks = [{
+    id: 1,
+    title: 'Google',
+    url: 'www.google.com',
+    description: 'This is the most popular search engine in the world',
+    rating: 5
+}]
+
+module.exports = bookmarks
